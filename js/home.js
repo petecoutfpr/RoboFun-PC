379a3f1fe4515f4490667c33d18e1cd56191a9e1
dericksa <derickball9@hotmail.com>
2018-06-14 14:51:00 -0300

    Inicial

---
+++ js/home.js
@@ -0,0 +1,7 @@
+var simulador = document.getElementById("Simulador");
+var fluxograma = document.getElementById("Simulador");
+var fonte = document.getElementById("Simulador");
+
+simulador.addEventListener("click", function(e) {
+    alert('Abrir página');
+}, false);
